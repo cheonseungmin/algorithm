004749eb8c268fc58a975950caa2128d1566b0ad
Cheon seungmin <dudghsx12@gmail.com>
2020-10-19 03:49:02 +0900

    03:00 passed

--- 프로그래머스/lv2/오픈채팅방.js
+++ 프로그래머스/lv2/오픈채팅방.js
@@ -1,41 +1,87 @@
-// https://programmers.co.kr/learn/courses/30/lessons/42888
+// https://programmers.co.kr/learn/courses/30/lessons/42890?language=javascript
 
-let records = [
-    "Enter uid1234 Muzi",
-    "Enter uid4567 Prodo",
-    "Leave uid1234",
-    "Enter uid1234 Prodo",
-    "Change uid4567 Ryan"
-];
+//let relation = [
+//    ["100", "ryan", "music", "2"],
+//    ["200", "apeach", "math", "2"],
+//    ["300", "tube", "computer", "3"],
+//    ["400", "con", "computer", "4"],
+//    ["500", "muzi", "music", "3"],
+//    ["600", "apeach", "music", "2"]
+//];
 
-let result = [
-    "Prodo님이 들어왔습니다.",
-    "Ryan님이 들어왔습니다.",
-    "Prodo님이 나갔습니다.",
-    "Prodo님이 들어왔습니다."
-];
+let relation = [
+    ['b', '2', 'a', 'a', 'b'],
+    ['b', '2', '7', '1', 'b'],
+    ['1', '0', 'a', 'a', '8'],
+    ['7', '5', 'a', 'a', '9'],
+    ['3', '0', 'a', 'f', '9']
+]
 
-let chats = [];
-let users = {};
+//let relation = [
+//    ['a', '1', '2'],
+//    ['5', '1', '5'],
+//    ['a', '2', '4']
+//]
 
-records.forEach(record => {
-    let s = record.split(' ');
-    if (s[0] === 'Enter') {
-        users[s[1]] = s[2];
-        chats.push(`${s[1]}님이 들어왔습니다.`);
-    } else if (s[0] === 'Leave') {
-        chats.push(`${s[1]}님이 나갔습니다.`);
-    } else {
-        users[s[1]] = s[2];
+let columns = relation[0].length;
+
+let propArr = Array(columns).fill(false);
+let checkArr = Array(columns).fill(false);
+
+let keys = [];
+
+const comb = (start, r) => {
+    if (r === 0) {
+        checkKey();
+        return;
     }
-});
+
+    for (let i = start; i < columns; i++) {
+        checkArr[i] = true;
+
+        comb(i + 1, r - 1);
+
+        checkArr[i] = false;
+    }
+
+}
 
 
-chats = chats.map((chat, i) => {
-    let j = chat.indexOf('님');
-    let s = chat.substring(0, j);
-    return users[s] + chat.substring(j, chat.length);
-})
+const checkKey = () => {
+    let tuples = relation.map(r => {
+        return r.reduce((acc, cur, i) => {
+            if (checkArr[i] === true) acc += '+' + cur;
+            return acc;
+        }, '');
+    });
 
+    let countArr = tuples.map(tuple => {
+        return tuples.filter(t => t === tuple).length;
+    });
 
-console.log(chats);
+    if (countArr.every(e => e === 1)) {
+        let key = checkArr.reduce((acc, check, i) => {
+            if (check === true) acc.push(i);
+            return acc;
+        }, []);
+
+        if (keys.length === 0) {
+            keys.push(key)
+        } else {
+            let flag = true;
+            for (let i = 0; i < keys.length; i++) {
+                let count = 0;
+                for (let j = 0; j < keys[i].length; j++) {
+                    if (key.includes(keys[i][j])) count++;
+                }
+                if (count === keys[i].length) flag = false;
+            }
+            if (flag) keys.push(key);
+        }
+    }
+}
+
+for (let i = 1; i <= columns; i++) comb(0, i);
+
+console.log(keys)
+console.log(keys.length)
