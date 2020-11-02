14f39c3d3aa37dc0a2dafd780a28605bd6efdaac
Cheon seungmin <dudghsx12@gmail.com>
2020-11-02 11:17:50 +0900

    00:05 passed

---
+++ 프로그래머스/lv1/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,13 @@
+let strings = ["sun", "bed", "car"]
+let n = 1;
+
+strings.sort((a, b) => {
+    if (a[n] < b[n]) return -1;
+    else if(a[n] > b[n]) return 1;
+    else {
+        if(a < b) return -1;
+        else return 1;
+    }
+});
+
+console.log(strings)
